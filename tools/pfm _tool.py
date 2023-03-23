8d4e3c2e8bf6f07d93694d8ead4f60abd65d1255
pelegcaduri <peleg.caduri@huji.mail.ac.il>
2023-03-23 15:20:44 +0200

    updated pfm tool

--- tools/pfm _tool.py
+++ tools/pfm _tool.py
@@ -11,8 +11,8 @@ INT16_MAX = 65536
 # import imageio
 # imageio.plugins.freeimage.download()
 def pfm2np(path):
-    a=  np.asarray(iio.imread(path))
-    return a
+    np.asarray(iio.imread(path))
+
 
 def display_pfm(path):
     display_np(pfm2np(path))
@@ -29,16 +29,20 @@ def convert_dtype_to_int16(image, scaling_factor=1000.0):
         for j in range(len(depth_image[0])):
             if depth_image[i][j] >= INT16_MAX:
                 depth_image[i][j] = INT16_MAX - 1
-    return depth_image
+    return np.flip(depth_image, axis=0)
 
 
 # Performs conversion of .pfm file to a pcd object using int16 data type
 def pfm2pcd(path, depth_trunc=20.0, intrinsic=DEFAULT_INTRINSIC):
+    return depth_np2pcd(pfm2np(path),depth_trunc,intrinsic)
+
+
+# Performs conversion of numpy depth map to a pcd object using int16 data type
+def depth_np2pcd(depth_np, depth_trunc=20.0, intrinsic=DEFAULT_INTRINSIC):
     depth_scale = INT16_MAX / (depth_trunc + 1)
-    depth = o3d.geometry.Image(np.array(convert_dtype_to_int16(pfm2np(path), depth_scale), dtype=np.uint16))
+    depth = o3d.geometry.Image(np.array(convert_dtype_to_int16(depth_np, depth_scale), dtype=np.uint16))
     return o3d.geometry.PointCloud.create_from_depth_image(depth, intrinsic, depth_scale=depth_scale,
                                                            depth_trunc=depth_trunc)
-
 
 def pfm2ply(input_path, output_path, depth_trunc=20.0, intrinsic=DEFAULT_INTRINSIC):
     o3d.io.write_point_cloud(output_path,pfm2pcd(input_path,depth_trunc,intrinsic))
@@ -47,6 +51,3 @@ def pfm2ply(input_path, output_path, depth_trunc=20.0, intrinsic=DEFAULT_INTRINS
 def pfm2greyscale(path, depth_trunc=20.0):
     depth_scale = INT16_MAX / (depth_trunc + 1)
     iio.imwrite('grayscale.png', np.array(convert_dtype_to_int16(pfm2np(path), depth_scale), dtype=np.uint16))
-
-o3d.visualization.draw([pfm2pcd('../tree.pfm')])
-pfm2ply('tree.pfm','tree.ply')
