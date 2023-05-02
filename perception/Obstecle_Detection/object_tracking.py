7bff722a0f74872e41c1241c9229879438595897
talgo43 <tal.gordon.43@gmail.com>
2023-05-02 23:21:35 +0300

    added obstecle detection module

---
+++ perception/Obstecle_Detection/object_tracking.py
@@ -0,0 +1,100 @@
+#  Object tracking for obstacle detection
+# ----------------- VERSION KCF ------------------------------
+import cv2
+import sys
+
+# (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+def rescale_frame(frame, percent=75):
+    width = int(frame.shape[1] * percent / 100)
+    height = int(frame.shape[0] * percent / 100)
+    dim = (width, height)
+    return cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+
+
+if __name__ == '__main__':
+
+    # Set up tracker.
+    # Instead of MIL, you can also use
+    #
+    # tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+    # tracker_type = tracker_types[3]
+    #
+    # if tracker_type == 'BOOSTING':
+    #     tracker = cv2.TrackerBoosting_create() - SLOW
+    # if tracker_type == 'MIL':
+    #     tracker = cv2.TrackerMIL_create() - SLOW
+    # if tracker_type == 'KCF':
+    #     tracker = cv2.TrackerKCF_create() - FAST, NOT CHAINING SIZE
+    # if tracker_type == 'TLD':
+    #     tracker = cv2.TrackerTLD_create()- SUPER SLOW  - "Also, tracks best over scale changes"?
+    # if tracker_type == 'MEDIANFLOW':
+    #     tracker = cv2.TrackerMedianFlow_create() - FAST, NOT SO ACCURATE, CHAINING SIZE
+    # if tracker_type == 'GOTURN':
+    #     tracker = cv2.TrackerGOTURN_create() - NOT WORKING, FIX?
+    # if tracker_type == 'MOSSE':
+    #     tracker = cv2.TrackerMOSSE_create() - REALLY FAST, NOT CHAINING SIZE
+    # if tracker_type == "CSRT":
+    #     tracker = cv2.TrackerCSRT_create() - SLOW
+    tracker = cv2.legacy.TrackerMedianFlow_create()
+    # Read video
+    video = cv2.VideoCapture("DJI_0457.MP4")
+    video.set(cv2.CAP_PROP_POS_MSEC, 3e4)
+    # Exit if video not opened.
+    if not video.isOpened():
+        print("Could not open video")
+        sys.exit()
+
+    # Read first frame.
+    ok, frame = video.read()
+    if not ok:
+        print('Cannot read video file')
+        sys.exit()
+
+    # Define an initial bounding box
+    bbox = (287, 23, 86, 320)
+
+    # Uncomment the line below to select a different bounding box
+    bbox = cv2.selectROI(frame, False)
+
+    # Initialize tracker with first frame and bounding box
+    ok = tracker.init(frame, bbox)
+
+    while True:
+        # Read a new frame
+        ok, frame = video.read()
+        if not ok:
+            break
+
+        # Start timer
+        timer = cv2.getTickCount()
+
+        # Update tracker
+        ok, bbox = tracker.update(frame)
+
+        # Calculate Frames per second (FPS)
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+
+        # Draw bounding box
+        if ok:
+            # Tracking success
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+        else:
+            # Tracking failure
+            cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+
+        # Display tracker type on frame
+        cv2.putText(frame, "KCF Tracker", (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+
+        # Display FPS on frame
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+
+        # Display result
+        cv2.imshow("Tracking", frame)
+
+        # Exit if ESC pressed
+        k = cv2.waitKey(1) & 0xff
+        if k == 27:
+            break
+# ----------------- VERSION MOSSE ------------------------------
