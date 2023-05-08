7139131f29a6ebe1a5a8be869ae73bc0f12af37c
RonelYaron <ronel.yaron.44@gmail.com>
2023-05-08 12:54:26 +0300

    added contour merging

--- perception/Obstacle_Detection/edge_detection.py
+++ perception/Obstacle_Detection/edge_detection.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
-
+from contour_merge import *
 
 def detect_edge():
     # Read the original image
-    img = cv2.imread("tree_img.jpg")
+    img = cv2.imread("photo_tree_2.jpg")
     # Display original image
-    cv2.imshow("tree_img.jpg", img)
+    cv2.imshow("photo_tree_2.jpg", img)
     cv2.waitKey(0)
 
     # Convert to graycsale
@@ -37,6 +37,7 @@ def detect_edge():
     # since findContours alters the image
     contours, hierarchy = cv2.findContours(edges,
                                            cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours = agglomerative_cluster(contours)
     cnt1 = contours[0]
     # cv2.imshow('Canny Edges After Contouring', edges)
     cv2.waitKey(0)
@@ -67,44 +68,3 @@ def detect_edge():
 
     cv2.destroyAllWindows()
 
-
-def calculate_contour_distance(contour1, contour2):
-    x1, y1, w1, h1 = cv2.boundingRect(contour1)
-    c_x1 = x1 + w1 / 2
-    c_y1 = y1 + h1 / 2
-
-    x2, y2, w2, h2 = cv2.boundingRect(contour2)
-    c_x2 = x2 + w2 / 2
-    c_y2 = y2 + h2 / 2
-
-    return max(abs(c_x1 - c_x2) - (w1 + w2) / 2, abs(c_y1 - c_y2) - (h1 + h2) / 2)
-
-
-def merge_contours(contour1, contour2):
-    return np.concatenate((contour1, contour2), axis=0)
-
-
-def agglomerative_cluster(contours, threshold_distance=40.0):
-    current_contours = contours
-    while len(current_contours) > 1:
-        min_distance = None
-        min_coordinate = None
-
-        for x in range(len(current_contours) - 1):
-            for y in range(x + 1, len(current_contours)):
-                distance = calculate_contour_distance(current_contours[x], current_contours[y])
-                if min_distance is None:
-                    min_distance = distance
-                    min_coordinate = (x, y)
-                elif distance < min_distance:
-                    min_distance = distance
-                    min_coordinate = (x, y)
-
-        if min_distance < threshold_distance:
-            index1, index2 = min_coordinate
-            current_contours[index1] = merge_contours(current_contours[index1], current_contours[index2])
-            del current_contours[index2]
-        else:
-            break
-
-    return current_contours
