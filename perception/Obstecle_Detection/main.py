5f7ee7335abe1a62ab6d3c9cc10cc9c11befd2ce
talgo43 <tal.gordon.43@gmail.com>
2023-05-08 08:49:58 +0300

    remove junk dir

--- perception/Obstecle_Detection/main.py
+++
@@ -1,5 +0,0 @@
-from masking import get_hsv_threshold, video_masking
-from object_tracking import single_object_tracking, multiple_obejcts_tracking
-if __name__ == '__main__':
-   single_object_tracking("../../data/real drone camera/DJI_0458.MP4")
-    #get_hsv_threshold()
