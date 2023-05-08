043659e8f7df9dd86e022a65b0044a71301ae514
nitzanami <nitzan.amihai1@gmail.com>
2023-05-08 18:00:52 +0300

    added a and b for weights

--- perception/horizon_detection/create_histogram.py
+++ perception/horizon_detection/create_histogram.py
@@ -1,24 +1,28 @@
 import copy
 import math
+import os
+
 import numpy as np
 import open3d as o3d
 import tools.pfm_tool as pfm_tool
 import time
 
-image_path = "../../data/basic enviroment/simulation data/img_ComputerVision_1_2_1679521978574868600.pfm"
-# image_path = "../../data/basic enviroment/blitz/single_tree.pfm"
+# image_path = "../../data/basic enviroment/simulation data/img_ComputerVision_1_2_1679521978574868600.pfm"
+image_path = '../../data/complex/img_SimpleFlight_1_2_1679841217449199400.pfm'
+# image_path = '../../data/complex/img_SimpleFlight_1_2_1679841217189484200.pfm'
 
 
 intrinsic_matrix = copy.deepcopy(pfm_tool.DEFAULT_INTRINSIC.intrinsic_matrix)
-# intrinsic_matrix[0, 2], intrinsic_matrix[1, 2] = 450, 350
+intrinsic_matrix[0, 2], intrinsic_matrix[1, 2] = 450, 350
 
 
 phi_min = 0
 phi_max = 0
 theta_min = 0
 theta_max = 0
-robot_size = 0.5
-
+robot_size = 0.3
+b = 0.08
+a = 1
 
 def get_theta_phi(x, y, z):
     return math.atan(x / z), math.atan(y / math.sqrt(x ** 2 + z ** 2))
@@ -116,13 +120,15 @@ def pcd_from_np_fast(depth_image: np.ndarray, max_distance: int):
     points = points[bools, :]
     pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(points))
     return pcd
-
+voxels = []
 
 def add_leaf_to_histogram(node: o3d.geometry.OctreeNode, node_info: o3d.geometry.OctreeNodeInfo, max_depth,
                           histogram: np.ndarray, theta_min, theta_max, phi_min, phi_max):
     ver, hor = histogram.shape
+    global voxels
     if node_info.depth != max_depth:
         return
+    voxels.append(len(node.indices))
     x, y, z = node_info.origin
     theta, phi = get_theta_phi(x, y, z)
     angle_range = math.asin((robot_size + node_info.size) / math.sqrt(x * x + y * y + z * z))
@@ -131,7 +137,8 @@ def add_leaf_to_histogram(node: o3d.geometry.OctreeNode, node_info: o3d.geometry
     ver_index_diff = math.floor(ver * angle_range)
     hor_index_diff = math.floor(hor * angle_range)
     add_in_angle_range(histogram, phi_index, theta_index, ver_index_diff, hor_index_diff,
-                       len(node.indices) * len(node.indices))
+                       (len(node.indices) * len(node.indices)) * (max([0,a - b * (math.sqrt(x*x+y*y+z*z) - robot_size)]))
+)
 
 
 def get_histogram(depth_image: np.ndarray, shape, max_distance=15, octree_depth=7, visualize=False):
@@ -146,7 +153,6 @@ def get_histogram(depth_image: np.ndarray, shape, max_distance=15, octree_depth=
     """
     es1 = time.time()
     pcd = pcd_from_np_fast(depth_image, max_distance)
-    # pcd = pfm_tool.depth_np2pcd(depth_image,max_distance)
     es2 = time.time()
     octree = o3d.geometry.Octree(max_depth=octree_depth)
     octree.convert_from_point_cloud(pcd, size_expand=0.01)
@@ -159,9 +165,6 @@ def get_histogram(depth_image: np.ndarray, shape, max_distance=15, octree_depth=
     octree.traverse(
         lambda x, y: add_leaf_to_histogram(x, y, octree_depth, histogram, theta_min, theta_max, phi_min, phi_max))
 
-    # def pass1():
-    #     pass
-    # octree.traverse(lambda x,y: pass1())
     es5 = time.time()
     print("point cloud: ", es2 - es1)
     print("octree: ", es3 - es2)
@@ -176,11 +179,19 @@ def get_histogram(depth_image: np.ndarray, shape, max_distance=15, octree_depth=
 
 
 def main():
+    # directory = "../../data/complex"
+    # for filename in os.listdir(directory):
+    #     print(filename)
+    #     arr = np.clip(pfm_tool.pfm2np(directory + '/' + filename),0,100)
+    #     pfm_tool.display_np(arr)
     depth_image = pfm_tool.pfm2np(image_path)
-    # depth_image = np.concatenate([np.ones((350, 900)) * 4, np.ones((350, 900))], axis=0)
-    depth_image = depth_image[300:500, 300:700]
-    hist = get_histogram(depth_image, (100, 200), 15, octree_depth=8, visualize=True)
-    pfm_tool.display_np(pfm_tool.convert_to_binary(hist, threshhold=15))
+    # depth_image = depth_image[300:500, 300:700]
+    hist = get_histogram(depth_image, (350,450), 20, octree_depth=7, visualize=True)
+    pfm_tool.display_np(pfm_tool.convert_to_binary(hist, threshhold=10))
+    voxels.sort(reverse=True)
+    for i in range (1000):
+        print(voxels[i])
+    print(len(voxels))
 
 
 if __name__ == "__main__":
