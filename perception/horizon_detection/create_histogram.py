533dbeecac94850e5ff703b5e1c9f9de3366a189
pelegcaduri <peleg.caduri@huji.mail.ac.il>
2023-05-08 08:51:16 +0300

    slow code, a little optmized

--- perception/horizon_detection/create_histogram.py
+++ perception/horizon_detection/create_histogram.py
@@ -3,13 +3,15 @@ import math
 import numpy as np
 import open3d as o3d
 import tools.pfm_tool as pfm_tool
+import time
 
 image_path = "../../data/basic enviroment/simulation data/img_ComputerVision_1_2_1679521978574868600.pfm"
 # image_path = "../../data/basic enviroment/blitz/single_tree.pfm"
 
 
 intrinsic_matrix = copy.deepcopy(pfm_tool.DEFAULT_INTRINSIC.intrinsic_matrix)
-intrinsic_matrix[0, 2], intrinsic_matrix[1, 2] = 450, 350
+# intrinsic_matrix[0, 2], intrinsic_matrix[1, 2] = 450, 350
+intrinsic_matrix[0, 2], intrinsic_matrix[1, 2] = 100, 200
 
 
 def get_theta_phi(x, y, z):
@@ -56,14 +58,29 @@ def pcd_from_np(depth_image, trunk=20):
     CX_DEPTH, CY_DEPTH = d[0, 2], d[1, 2]
     height, width = depth_image.shape
     points = []
+    ind = [0,0]
+    def add_point(depth):
+        if depth < trunk:
+            ind[1] += 1
+            if ind[1] == height:
+                ind[0] += 1
+                ind[1] = 0
+            x_fac = (500 - CX_DEPTH) / FX_DEPTH
+            y_fac = (500 - CY_DEPTH) / FY_DEPTH
+            z = depth / (math.sqrt(1 + x_fac * x_fac + (y_fac * y_fac)))
+            points.append([z * x_fac, -z * y_fac, z])
+    np.vectorize(add_point)(depth_image)
+
     for i in range(height):
         for j in range(width):
-            z = depth_image[i, j] / (math.sqrt(1 + ((j - CX_DEPTH) / FX_DEPTH) ** 2 + ((i - CY_DEPTH) / FY_DEPTH) ** 2))
-            x = z * (j - CX_DEPTH) / FX_DEPTH
-            y = -z * (i - CY_DEPTH) / FY_DEPTH
-            if x ** 2 + y ** 2 + z ** 2 < trunk ** 2:
-                points.append([x, y, z])
+            if depth_image[i, j] < trunk:
+                x_fac = (j - CX_DEPTH) / FX_DEPTH
+                y_fac = (i - CY_DEPTH) / FY_DEPTH
+                z = depth_image[i, j] / (math.sqrt(1 + x_fac * x_fac + (y_fac * y_fac)))
+                points.append([z * x_fac, -z * y_fac, z])
+    x = time.process_time()
     pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(points))
+    print("pcd:",time.process_time()-x)
     return pcd
 
 
@@ -83,8 +100,7 @@ def add_leaf_to_histogram(node: o3d.geometry.OctreeNode, node_info: o3d.geometry
     add_in_angle_range(histogram, phi_index, theta_index, ver_index_diff, hor_index_diff, len(node.indices) ** 2)
 
 
-def get_histogram(depth_image: np.ndarray, shape: tuple[int, int], max_distance=15, octree_depth=7, drone_size=0.3,
-                  visualize=False):
+def get_histogram(depth_image: np.ndarray, shape, max_distance=15, octree_depth=7, drone_size=0.3, visualize=False):
     """
     create polar histogram from the given image.
     :param depth_image:
@@ -95,15 +111,24 @@ def get_histogram(depth_image: np.ndarray, shape: tuple[int, int], max_distance=
     :param visualize: True if we want to display the depth_map and the resulting histogram
     :return:
     """
+    es1 = time.process_time()
     pcd = pcd_from_np(depth_image, max_distance)
+    # pcd = pfm_tool.depth_np2pcd(depth_image,max_distance)
+    es2 = time.process_time()
     octree = o3d.geometry.Octree(max_depth=octree_depth)
     octree.convert_from_point_cloud(pcd, size_expand=0.01)
+    es3 = time.process_time()
     histogram = np.zeros(shape)
     min_theta, min_phi = get_theta_phi_from_point(get_point_from_image(depth_image.shape[0] - 1, 0, depth_image))
     max_theta, max_phi = get_theta_phi_from_point(get_point_from_image(0, depth_image.shape[1] - 1, depth_image))
-
+    es4 = time.process_time()
     octree.traverse(lambda x, y: add_leaf_to_histogram(
         x, y, octree_depth, histogram, min_phi, max_phi, min_theta, max_theta, drone_size))
+    es5 = time.process_time()
+    print(es2 - es1)
+    print(es3 - es2)
+    print(es4 - es3)
+    print(es5 - es4)
     if visualize:
         pfm_tool.display_np(np.clip(depth_image, 0, max_distance))
         pfm_tool.display_np(histogram)
@@ -112,7 +137,9 @@ def get_histogram(depth_image: np.ndarray, shape: tuple[int, int], max_distance=
 
 def main():
     depth_image = pfm_tool.pfm2np(image_path)
-    get_histogram(depth_image, (500, 500), 15, drone_size=0.3, octree_depth=8, visualize=True)
+    depth_image = depth_image
+    hist = get_histogram(depth_image, (500,500), 15, drone_size=0.5, octree_depth=8, visualize=True)
+    pfm_tool.display_np(pfm_tool.convert_to_binary(hist, threshhold=10))
 
 
 if __name__ == "__main__":
