fixed angle calculation in get_direction_from_image()

perception/horizon_detection/find_direction.py
@@ -6,8 +6,11 @@ from tools import pfm_tool
 
 
 def get_direction_from_image(image: np.ndarray):
-    hist = get_histogram(image, (70, 90), 20, 7, weight_func=cos_weight)
+    res = (70, 90)
+    hist = get_histogram(image, res, 20, 7, weight_func=cos_weight)
     thresh = np.nanpercentile(hist[hist != 0], 10) * 2
     binary = pfm_tool.convert_to_binary(hist, thresh)
     i, j = binary_find_windows(binary, simple_loss_function)
-    return get_theta_phi_from_index(i, j)
+    theta_min, phi_min = get_theta_phi_from_index(image.shape[0] - 1, 0)
+    theta_max, phi_max = get_theta_phi_from_index(0, image.shape[1] - 1)
+    return theta_min + j / res[1] * (theta_max - theta_min), phi_min + (phi_max - phi_min) * i / res[0]